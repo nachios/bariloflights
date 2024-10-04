c69a13933acbf22018cd1974521810a4fbba9533
Nacho S <nacho.santangelo@gmail.com>
2024-10-04 00:12:55 -0300

    Format edits for flake8

--- tests/test_distinct_flights.py
+++ tests/test_distinct_flights.py
@@ -1,16 +1,15 @@
 import pandas as pd
-import pytest
 from unittest.mock import patch, MagicMock
 from dags.scripts.distinct_flights import distinct_flight_data
 
 # Sample data to mock the SQL table output
 sample_data = pd.DataFrame({
     'flight_code': ['FO123', 'FO242', 'AA213'],
-    'departure_date': ['2024-09-28 00:00:00', '2024-09-28 00:00:00', '2024-09-28 00:00:00'], 
+    'departure_date': ['2024-09-28 00:00:00', '2024-09-28 00:00:00', '2024-09-28 00:00:00'],
     'origin': ['BRC', 'BRC', 'BRC'],
     'destination': ['AEP', 'AEP', 'AEP'],
     'airline_code': ['FO', 'FO', 'AA'],
-    'snapshot_date': ['2024-09-28 00:00:00', '2024-09-25 00:00:00', '2024-09-28 00:00:00'] 
+    'snapshot_date': ['2024-09-28 00:00:00', '2024-09-25 00:00:00', '2024-09-28 00:00:00']
 })
 
 # Expected output after processing
@@ -23,6 +22,7 @@ expected_data = pd.DataFrame({
     'is_active': [1, 0, 1]
 })
 
+
 @patch('dags.scripts.distinct_flights.create_engine')
 @patch('pandas.read_sql_table', return_value=sample_data)
 @patch('airflow.hooks.base.BaseHook.get_connection')
@@ -33,7 +33,7 @@ def test_distinct_flight_data(mock_get_connection, mock_read_sql_table, mock_cre
     mock_connection.password = 'password'
     mock_connection.host = 'localhost'
     mock_connection.port = '5439'
-    mock_connection.schema = 'your_schema'
+    mock_connection.schema = 'schema'
     mock_get_connection.return_value = mock_connection
 
     # Mocking the engine's connect method to avoid actual database connection
